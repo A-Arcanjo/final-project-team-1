27f1b19b8c7adfa8c0fbca454898419a17220aea
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-07-18 16:07:10 +0200

    services

--- front-end/src/components/pages/Home/Home.js
+++ front-end/src/components/pages/Home/Home.js
@@ -6,6 +6,7 @@ import image4 from "../../images/image4.jpg";
 import image5 from "../../images/image5.jpg";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import bär from "../../images/image5.jpg";
 
 const Home = () => {
     return (
@@ -19,9 +20,10 @@ const Home = () => {
                 <img alt="" src={image5} className="slider-img" />
 
             </Carousel>
+            <div className="services">
+                <img alt="" src={bär} className="services_pic" />
 
-
-
+            </div>
             <style jsx>{`
  .slider-img {
    width: 100%;
