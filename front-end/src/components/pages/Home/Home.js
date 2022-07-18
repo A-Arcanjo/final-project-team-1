1ace4fb04288cb9a24f50e62ab0e358ecd53d706
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-07-18 15:24:41 +0200

    slide

--- front-end/src/components/pages/Home/Home.js
+++ front-end/src/components/pages/Home/Home.js
@@ -1,9 +1,31 @@
 import React from "react";
+import image1 from "../../images/image1.jpg";
+import image2 from "../../images/image2.jpg";
+import image3 from "../../images/image3.jpg";
+import image4 from "../../images/image4.jpg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
     return (
-        <div className="">Home</div>
-    )
-}
+        <>
+            <Carousel showArrows={true} showThumbs={true} autoPlay autoPlayInterval="3000">
+                <div className="">Home </div >
+                <img alt="" src={image1} className="slider-img" />
+                <img alt="" src={image2} className="slider-img" />
+                <img alt="" src={image3} className="slider-img" />
+                <img alt="" src={image4} className="slider-img" />
+            </Carousel>
+
+            <style jsx>{`
+ .slider-img {
+   width: 100%;
+   height: 500px;
+   object-fit: cover;
+ }
+`}</style>
+        </>
+    );
+};
 
 export default Home;
