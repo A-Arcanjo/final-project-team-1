28febb4a7b18337034644797bded250b20e1c4d1
Mohammed <atwan.m1990@gmail.com>
2022-07-19 15:10:55 +0200

    first-sprint

--- front-end/src/components/pages/Signup/Signup.js
+++ front-end/src/components/pages/Signup/Signup.js
@@ -6,13 +6,13 @@ const Signup = () => {
         <div className="signup-container">
             <div className="main-signup">
                 <h2>Choose an account</h2>
-                <div className="box">
+                <div className="sign-box">
                     <a href="business-signup">
                         <h3>Business account</h3>
                         <p>We are aiming to upgrade your business and help you to reach out more customers.</p>
                     </a>
                 </div>
-                <div className="box">
+                <div className="sign-box">
                     <a href="customer-signup">
                         <h3>Customer</h3>
                         <p>We're giving you the most easy way to reach out what you are looking for.</p>
