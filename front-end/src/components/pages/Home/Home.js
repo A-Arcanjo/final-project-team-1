7f9130338178a2065568ff984e3d8981dbc30d09
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-07-19 11:31:10 +0200

    partners

--- front-end/src/components/pages/Home/Home.js
+++ front-end/src/components/pages/Home/Home.js
@@ -10,6 +10,8 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import chef from "../../images/chef.png";
 import chef2 from "../../images/chef2.jpg";
+import cow from "../../images/cow.png";
+
 
 const Home = () => {
     return (
@@ -24,8 +26,9 @@ const Home = () => {
                 <img alt="" src={iceCream} className="slider-img" />
 
             </Carousel>
+            <h2 className="text">Services</h2>
             <div className="services-container">
-                <h2 className="text">Services</h2>
+
                 <img alt="" src={chef} className="chef" />
 
                 <div className="services">
@@ -66,44 +69,96 @@ const Home = () => {
 
                 <img alt="" src={chef2} className="chef2" />
             </div>
+
+            <div className="services-container">
+                <img alt="" src={cow} className="cow" />
+
+                <div className="services">
+
+                    <div className="box">
+                        <p> ipsedixit
+                        </p>
+                    </div>
+                    <div className="box">
+                        <p> ipsedixit
+                        </p>
+                    </div>
+                    <div className="box">
+                        <p> ipsedixit
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="partner-container">
+                <div className="partners">
+
+                    <div className="box-partner">
+                        <p> Partner
+                        </p>
+                    </div>
+                    <div className="box-partner">
+                        <p> Partner
+                        </p>
+                    </div>
+                    <div className="box-partner">
+                        <p> Partner
+                        </p>
+                    </div>
+                    <div className="box-partner">
+                        <p> Partner
+                        </p>
+                    </div>
+                    <div className="box-partner">
+                        <p> Partner
+                        </p>
+                    </div>
+                </div>
+
+            </div>
             <style jsx>{`
- .slider-img {
-   width: 100%;
-   height: 500px;
-   object-fit: cover;
- }
+.slider-img {
+    width: 100%;
+    height: 500px;
+    object-fit: cover;
+}
 
- .services{
+.services{
     height: 100%;
-          width: 400px;
-          display: flex;
-          justify-content: space-around;
-          flex-direction:column;
-         
- }
+    display: flex;
+    justify-content: space-around;
+    flex-direction:column;    
+}
 
- .chef{
+.chef{
     width: 20%;
-   height: 200px;
-   object-fit:fit;
- }
+    height: 200px;
+    object-fit:fit;
+}
 
- .chef2{
-    
+.chef2{
     width: 30%;
-   height: 200px;
-   object-fit:fit;
- }
+    height: 200px;
+    object-fit:fit;
+}
 
+.cow{
+    width: 20%;
+    height: 200px;
+    object-fit:fit;
+}
 
- }
+}
 
- .text{
+.text{
     color:#31525b;
     text-align: center;
- }
+    margin-top:40px;
+}
 
-    .services-container {
+.services-container {
     display: flex;
     width:100%;
     flex-direction: row;
@@ -117,7 +172,6 @@ const Home = () => {
 
 .box {
     width: 400px;
-    border: 1px solid #31525b;
     padding: 20px;
     margin-top:10px;
     margin-bottom:10px;
@@ -125,7 +179,227 @@ const Home = () => {
     background: #fae6b1;
     alignItems: center;
 }
+
+.partner-container {
+    width:100%;
+    margin: auto;
+    margin-top:30px;
+    padding:15px 10px;
+    flex: 1 1;
+    width: 100%;
+}
+
+.box-partner {
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    width: 200px;
+    height: 200px;
+    padding: 20px;
+    margin-top:10px;
+    margin-bottom:10px;
+    border-radius: 5px;
+    background: #fae6b1;
+}
+
+.partners{
+    height: 100%;
+    margin: 10px 20px;
+    display:flex;
+    flex-direction:row;
+    justify-content: space-around;
+}
+
+/*Responsive Start*/
+@media screen and (max-width: 900px){
+.box{
+    width: 20rem;
+    right: 8%;
+}
+
+.services-container {
+    display: flex;
+    flex-direction: row;
+    justify-content:space-around;
+    margin-top:30px;
+    padding:25px 50px;
+    flex: 1 1;
+    width: 100%;
+}
+
+
+.partner-container {
+   
+    width:100%;
+    margin: auto;
+    margin-top:30px;
+    padding:15px 10px;
+    flex: 1 1;
+    width: 100%;
+}
+
+.box-partner {
+   display:flex;
+   justify-content:space-around;
+   align-items:center;
+   width: 100px;
+   height: 100px;
+   padding: 20px;
+   margin-top:10px;
+   margin-bottom:10px;
+   border-radius: 5px;
+   background: #fae6b1;
+}
+
+.partners{
+    height: 100%;
+    margin: 10px 20px;
+    display:flex;
+    flex-direction:row;
+    justify-content: space-around;
+        
+}
+}
+@media screen and (max-width: 600px){
+.chef{
+    width: 40%;
+    height: 200px;
+    margin-right:20px;
+    object-fit:fit;
+}
+
+.chef2{
+    width: 50%;
+    height: 200px;
+    object-fit:fit;
+}
+
+.cow{
+    width: 30%;
+    height: 100px;
+    margin-right:10px;
+    object-fit:fit;
  }
+
+.box{
+    width: 10rem;
+}
+
+.services-container {
+    display: flex;
+    flex-direction: row;
+    justify-content:space-around;
+    margin-top:30px;
+    padding:25px 50px;
+    flex: 1 1;
+    width: 100%;
+}
+
+.partner-container {
+   
+   width:100%;
+   margin: auto;
+   margin-top:30px;
+   padding:15px 10px;
+   flex: 1 1;
+   width: 100%;
+}
+
+.box-partner {
+   display:flex;
+   justify-content:space-around;
+   align-items:center;
+   width: 60px;
+   height: 60px;
+   padding: 20px;
+   margin-top:10px;
+   margin-bottom:10px;
+   border-radius: 5px;
+   background: #fae6b1;
+}
+
+.partners{
+    height: 100%;
+    margin: 10px 20px;
+    display:flex;
+    flex-direction:row;
+    flex-wrap:wrap;
+    align-items:center;
+    justify-content: space-around;
+        
+}
+}
+@media screen and (max-width: 300px){
+
+.chef{
+    width: 50%;
+    height: 200px;
+    margin-right:20px;
+    object-fit:fit;
+}
+
+.chef2{
+    
+    width: 50%;
+    height: 100px;
+    object-fit:fit;
+}
+
+.cow{
+    width: 40%;
+    height: 100px;
+    margin-right:10px;
+    object-fit:fit;
+}
+
+.box{ 
+        width: 10rem;
+    }
+
+.services-container {
+    display: flex;
+    flex-direction: row;
+    justify-content:space-around;
+    margin-top:30px;
+    padding:25px 50px;
+    flex: 1 1;
+    width: 100%;
+}
+
+
+.partner-container {
+   
+   width:100%;
+   margin: auto;
+   margin-top:30px;
+   padding:15px 10px;
+   flex: 1 1;
+   width: 100%;
+}
+
+.box-partner {
+   display:flex;
+   justify-content:space-around;
+   align-items:center;
+   width: 40px;
+   height: 40px;
+   padding: 20px;
+   margin-top:10px;
+   margin-bottom:10px;
+   border-radius: 5px;
+   background: #fae6b1;
+}
+
+.partners{
+    height: 100%;
+    margin: 10px 20px;
+    display:flex;
+    flex-direction:row;
+    justify-content: space-around;
+        
+}
+}
+
 `}</style>
         </>
     );
