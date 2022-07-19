aa2547e6c8878042aa307cbe046722569fd57293
Mohammed <atwan.m1990@gmail.com>
2022-07-19 13:17:37 +0200

    edit responsive/ add business signup/ add contact

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,9 +5,11 @@ import Home from "./components/pages/Home/Home.js";
 import About from "./components/pages/About.js";
 import Services from "./components/pages/Services.js";
 import Rating from "./components/pages/Rating.js";
-import Contact from "./components/pages/Contact.js";
+import Contact from "./components/pages/Contact/Contact.js";
 import Signin from "./components/pages/Signin/Signin.js"
 import Signup from "./components/pages/Signup/Signup.js";
+import CustomerSignup from "./components/pages/Signup/CustomerSignup.js";
+import BusinessSignup from "./components/pages/Signup/BusinessSignup.js";
 import Client from "./components/pages/Client/Client.js";
 import Footer from "./components/Footer/Footer.js";
 import './App.css';
@@ -26,6 +28,8 @@ function App() {
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/signin" element={<Signin />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/customer-signup" element={<CustomerSignup />} />
+                <Route path="/business-signup" element={<BusinessSignup />} />
                 <Route path="/client" element={<Client />} />
             </Routes>
         </div>
