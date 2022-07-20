752e0e5508ed49ad2a0a30295d3d9913d044e769
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-07-20 17:29:28 +0200

    about

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.js";
 import Home from "./components/pages/Home/Home.js";
-import About from "./components/pages/About.js";
+import About from "./components/pages/About/About.js";
 import Services from "./components/pages/Services.js";
 import Rating from "./components/pages/Rating.js";
 import Contact from "./components/pages/Contact/Contact.js";
---
+++ front-end/src/components/pages/About/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Cards from "../../Cards/Cards";
+import blueberry1 from "../../images/blueberry1.jpeg";
+
+const About = () => {
+    return (
+        <>
+            <img alt="" src={blueberry1} className="blueberry" />
+            <Cards />
+        </>
+
+    );
+};
+
+export default About;
