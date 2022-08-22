a505f1fa5f115d3d826a42542b21ca8ca6ed0e11
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-08-22 12:08:25 +0200

    issue Branch userTypeBranch

--- backend/controllers/businessController.js
+++ backend/controllers/businessController.js
@@ -1,35 +1,33 @@
 import createError from "http-errors";
 import BusinessUser from "../models/businessUser.js";
 
-<<<<<<< HEAD
 export const getBusinessData = async (req, res, next) => {
-    // retrieve the :id parameter from the request
+  // retrieve the :id parameter from the request
 
-    const userId = req.params.id;
+  const userId = req.params.id;
 
-    let foundUser;
+  let foundUser;
 
-    try {
-        foundUser = await BusinessUser.findById(userId);
-    } catch {
-        return next(
-            createError.InternalServerError(
-                "Could not query database. Please try again!"
-            )
-        );
-    }
+  try {
+    foundUser = await BusinessUser.findById(userId);
+  } catch {
+    return next(
+      createError.InternalServerError(
+        "Could not query database. Please try again!"
+      )
+    );
+  }
 
-    if (foundUser) {
-        const userData = {
-            firstName: foundUser.firstName,
-        };
+  if (foundUser) {
+    const userData = {
+      firstName: foundUser.firstName,
+    };
 
-        res.json(userData);
-    } else {
-        return next(createError.NotFound("User could not be found."));
-    }
+    res.json(userData);
+  } else {
+    return next(createError.NotFound("User could not be found."));
+  }
 };
-=======
 // * GET the business user data 
 export const getUserData = async (req, res, next) => {
   // retrieve the :id parameter from the request
@@ -59,4 +57,3 @@ export const getUserData = async (req, res, next) => {
 };
 
 // * PATCH a new product
->>>>>>> b0bed9ec42ea41b722610fdec73139be03cc2a7a
