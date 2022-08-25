4e7f132b2c6f94f9cb2234c12989bcb1cd1b0b56
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-08-25 17:38:44 +0200

    fixedContainerXL

--- front-end/src/components/pages/Dashboard/HomeBusiness.jsx
+++ front-end/src/components/pages/Dashboard/HomeBusiness.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Posts from "../../Posts/Posts.js";
+import { getPosts } from "../../../actions/posts.js";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+import "../Home/Home.css";
+import makeStyles from "./styles";
+import Contact from "../Contact/Contact";
+import { Paper, Divider } from "@material-ui/core";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import Pagination from "../../../components/Pagination";
+import ContainerSearch from "../../../ContainerSearch.js";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffa101",
+      contrastText: "#31525b",
+    },
+  },
+});
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 export const HomeBusiness = () => {
-  return <div>This is the Home of HomeBusiness</div>;
+  const classes = makeStyles();
+
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const page = query.get("page") || 1;
+  const navigate = useNavigate();
+  const [currentId, setCurrentId] = useState(0);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+
+  return (
+    <div className="services">
+      <h2>Products</h2>
+      <div className="services-container">
+        <ThemeProvider theme={theme}>
+          <ContainerSearch />
+        </ThemeProvider>
+      </div>
+      <Contact />
+    </div>
+  );
 };
