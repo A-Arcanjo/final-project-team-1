0463620ed81b94fecce0a9bda65420975812dbb7
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-08-31 16:01:37 +0200

    Post changes

--- front-end/src/components/Posts/Post/Post.js
+++ front-end/src/components/Posts/Post/Post.js
@@ -64,7 +64,7 @@ const Post = ({ post, setCurrentId }) => {
                 <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
                 <div className={classes.overlay}>
                     <Typography variant="h6">{post.name}</Typography>
-                    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>  /created 1 minute ago
+                    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
                 </div>
                 {(currentUser.result?._id === post?.creator) && (
                     <div className={classes.overlay2} name="edit">
@@ -85,7 +85,7 @@ const Post = ({ post, setCurrentId }) => {
                 </div>
                 <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
                 <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 20).join(' ')}...</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 10).join(' ')}... <b style={{ color: 'black' }}>read more</b></Typography>
                 </CardContent>
             </ButtonBase>
             <CardActions style={{ padding: '4px' }} className={classes.cardActions}>
